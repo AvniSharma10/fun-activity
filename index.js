6432af2b1ff5a05f7e6ab716a02c4dea6fac0efe
Avni Sharma <117329971+AvniSharma10@users.noreply.github.com>
2024-07-21 17:09:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import bodyParser from "body-parser";
 import axios from "axios";
 
 const app = express();
-const port = const port = process.env.PORT || 3000; 
+const port = process.env.PORT || 3000; 
 
 app.set("view engine", "ejs");
 app.set("views", "views");
